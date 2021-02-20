06e7b63f436d53f00df7f1a48c79392f6697c3c5
Johnny Chan <chanyaoc@gmail.com>
2021-02-21 00:51:52 +0800

    Separate open and close positions

--- stock.py
+++ stock.py
@@ -7,11 +7,16 @@ class Stock:
         self.ticker = ticker
         self.transactions = []
         self.current_price = 0.0
-        self.current_asset = 0.0
-        self.capital_invested = 0.0
-        self.accumulated_gain = 0.0
+        self.open_capital_invested = 0.0
+        self.open_market_value = 0.0
+        self.open_accumulated_gain = 0.0
+        self.open_position = 0
+        self.open_return_rate = 0.0
         self.dividends_earned = 0.0
-        self.position = 0
+        self.close_capital_invested = 0.0
+        self.close_accumulated_gain = 0.0
+        self.close_return_rate = 0.0
+        self.close_position = 0
         self.total_gain = 0
 
     def __repr__(self):
@@ -30,28 +35,70 @@ class Stock:
         self._ticker = ticker
 
     def addTransaction(self, date, action, price, volume):
-        t = Transaction(date, action, price, volume)
+        t = Transaction(date, action, price, volume, 0)
         self.transactions.append(t)
         if t.action == 'B':
-            self.capital_invested += t.price * t.volume
-            self.position += t.volume
+            self._updateBuy(t)
         elif t.action == 'S':
-            self.accumulated_gain += t.price * t.volume
-            self.position -= t.volume
+            self._updateSell(t)
         elif t.action == 'D':
-            self.dividends_earned += t.price
+            self._updateDividend(t)
         else:
-            raise RuntimeError('Illegal transaction type')
+            raise RuntimeError('Illegal transaction type, only B/S/D allowed')
 
-    def getReturn(self):
-        self._calculateReturn()
-        return [self.ticker, self.total_gain, self.total_return]
-
-    def _calculateReturn(self):
+    def calculateReturn(self):
         self._getCurrentPrice()
-        self.current_asset = self.position * self.current_price + self.dividends_earned
-        self.total_gain = self.accumulated_gain - self.capital_invested + self.current_asset
-        self.total_return = self.total_gain / self.capital_invested
+        self._calculateCloseReturn()
+        self._calculateOpenReturn()
+
+    def returnOpen(self):
+        if self.open_position > 0:
+            return [self.ticker, self.open_position, self.open_accumulated_gain, self.open_return_rate]
+        return None
+
+    def returnClose(self):
+        if self.close_position > 0:
+            return [self.ticker, self.close_position, self.close_accumulated_gain, self.close_return_rate]
+        return None
+
+    def _updateBuy(self, new_trans):
+        self.open_capital_invested += new_trans.price * new_trans.volume
+        self.open_position += new_trans.volume
+
+    def _updateSell(self, new_trans):
+        shares_to_sell = new_trans.volume
+        for i in range(len(self.transactions)):
+            if self.transactions[i].action == 'B':
+                if self.transactions[i].volume > self.transactions[i].volume_traded and shares_to_sell > 0:
+                    shares_sold = 0
+                    if self.transactions[i].volume - self.transactions[i].volume_traded >= shares_to_sell:
+                        self.transactions[i].volume_traded += shares_to_sell
+                        shares_sold = shares_to_sell
+                        shares_to_sell = 0
+                    else:
+                        shares_to_sell -= self.transactions[i].volume - self.transactions[i].volume_traded
+                        self.transactions[i].volume_traded = self.transactions[i].volume
+                        shares_sold = self.transactions[i].volume - self.transactions[i].volume_traded
+                    self.close_accumulated_gain += shares_sold * (new_trans.price - self.transactions[i].price)
+                    self.close_capital_invested += shares_sold * self.transactions[i].price
+                    self.close_position += shares_sold
+                    self.open_position -= shares_sold
+                    self.open_capital_invested -= shares_sold * self.transactions[i].price
+                if shares_to_sell == 0:
+                    break
+
+    def _updateDividend(self, new_trans):
+        self.dividends_earned += new_trans.price
+
+    def _calculateCloseReturn(self):
+        if self.close_capital_invested > 0:
+            self.close_return_rate = self.close_accumulated_gain / self.close_capital_invested
+
+    def _calculateOpenReturn(self):
+        self.open_market_value += self.current_price * self.open_position
+        self.open_accumulated_gain = self.open_market_value - self.open_capital_invested
+        if self.open_capital_invested > 0:
+            self.open_return_rate = self.open_accumulated_gain / self.open_capital_invested
 
     def _getCurrentPrice(self):
         self._getInfoFromYFinance()
@@ -61,5 +108,5 @@ class Stock:
         try:
             self.info = yf.Ticker(self._ticker)
         except:
-            RuntimeError('Can not fetch stock info for "%s"', self._ticker)
+            RuntimeError('Cannot fetch stock info for "%s"', self._ticker)
 
